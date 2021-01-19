b564f26fbbfe0696be9a951d76d4fa3cb43a9881
kevinluuhuynh <kevin.luuhuynh@gmail.com>
2021-01-18 21:10:23 -0500

    added login fuction

--- script.js
+++ script.js
@@ -7,28 +7,28 @@
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  actionRequest: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
 
 const account2 = {
   owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  actionRequest: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
   owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  actionRequest: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
+  actionRequest: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
 };
@@ -61,53 +61,50 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovements = function (movements) {
+const displayActions = function (account) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdraw';
+  account.actionRequest.forEach(function (request, i) {
+    const type = request > 0 ? 'deposit' : 'withdrawal';
 
     const html = `
     <div class="movements__row">
-    <div class="movements__type movements__type--deposit">${i + 1} ${type}</div>
-    <div class="movements__date">3 days ago</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${Math.abs(request)}€</div>
     </div>`;
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
 };
 
-displayMovements(account1.movements);
-
-const calcPrintBalance = function (movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+const calcDisplayBalance = function (account) {
+  const balance = account.actionRequest.reduce(
+    (acc, request) => acc + request,
+    0
+  );
+  labelBalance.textContent = `${balance}€`;
 };
-calcPrintBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incoming = movements
-    .filter(mov => mov > 0)
-    .reduce((acc, mov) => acc + mov, 0);
+const calcDisplaySummary = function (account) {
+  const incoming = account.actionRequest
+    .filter(request => request > 0)
+    .reduce((acc, request) => acc + request, 0);
 
   labelSumIn.textContent = `${incoming}€`;
 
-  const outgoing = movements
-    .filter(mov => mov < 0)
-    .reduce((acc, mov) => acc + mov, 0);
+  const outgoing = account.actionRequest
+    .filter(request => request < 0)
+    .reduce((acc, request) => acc + request, 0);
 
   labelSumOut.textContent = `${Math.abs(outgoing)}€`;
 
-  const interest = movements
-    .filter(mov => mov > 0)
-    .map(deposit => deposit * (1.2 / 100))
+  const interest = account.actionRequest
+    .filter(request => request > 0)
+    .map(deposit => deposit * (account.interestRate / 100))
     .filter(int => int >= 1)
     .reduce((acc, int) => acc + int, 0);
 
   labelSumInterest.textContent = `${interest}€`;
 };
-
-calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accounts) {
   accounts.forEach(function (account) {
@@ -130,4 +127,23 @@ btnLogin.addEventListener('click', function (e) {
   currentAccount = accounts.find(
     acc => acc.username === inputLoginUsername.value
   );
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Welcome ${currentAccount.owner.split(' ')[0]}!`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Display movements
+    displayActions(currentAccount);
+
+    // Display balance
+    calcDisplayBalance(currentAccount);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
 });
