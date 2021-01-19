1e6fd4dcc99458c7a1e5c8c050de78eff9d21fa4
kevinluuhuynh <kevin.luuhuynh@gmail.com>
2021-01-19 01:12:09 -0500

    added closing account function

--- script.js
+++ script.js
@@ -179,3 +179,24 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+// Closing account function
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Check if correct credentials
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
